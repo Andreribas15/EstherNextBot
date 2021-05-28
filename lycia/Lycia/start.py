b3609a73745763f6e858a9bd411df5805b6aa5ec
Andreribas15 <54609210+Andreribas15@users.noreply.github.com>
2021-05-28 11:46:04 -0300

    Update start.py

--- lycia/Lycia/start.py
+++ lycia/Lycia/start.py
@@ -3,13 +3,13 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 LYCIA_START = """
-I am Lycia [リュキア], An Intelligent ChatBot
+Olá, eu sou a Esther. Uma inteligência artificial muito esperta.
 """
 
 
 @LYCIA.on_message(filters.command(["start"], prefixes = "/") & ~filters.edited)
 async def info(client, message):
     buttons = [
-                [InlineKeyboardButton("Lycia", switch_inline_query_current_chat="lycia "), InlineKeyboardButton("Github", url = "https://github.com/Red-Aura/Lyciachatbot")]
+                [InlineKeyboardButton("Inline", switch_inline_query_current_chat="lycia "), InlineKeyboardButton("Canal", url = "https://t.me/ARNextRobot")]
               ]
     await LYCIA.send_message(chat_id = message.chat.id, text = LYCIA_START, reply_markup = InlineKeyboardMarkup(buttons))
